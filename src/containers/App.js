7f8162ac8397d833d207fbdee6640b4a6decace5
Kevin Vu <hongkhanhvu94@gmail.com>
2025-01-06 19:37:14 +1100

    fix link on heading to point back to /pokedex

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router basename="/pokedex">
       <div className="bg-light">
-        <Link to="/">
+        <Link to="/pokedex">
           <Heading />
         </Link>
         <Routes>
